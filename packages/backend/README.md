567ecbb9025fbad552a5506c8b6e28e9b0610a5a
DURAND Malo <durand.malo.60590@gmail.com>
2025-11-03 14:22:46 -0500

    init: backend package (#19)

---
+++ packages/backend/README.md
@@ -0,0 +1,3 @@
+# Hallmaster backend
+
+NestJS application powered by Fastify for the Hallmaster backend.
---
+++ packages/backend/src/logger/logger.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, LoggerService as NestLoggerService } from '@nestjs/common';
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+
+@Injectable()
+export class LoggerService implements NestLoggerService {
+  private readonly logger: winston.Logger;
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'debug',
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('Nest', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+      ],
+    });
+  }
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string, trace: string) {
+    this.logger.error(message, trace);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+
+  verbose(message: string) {
+    this.logger.debug(message);
+  }
+}
