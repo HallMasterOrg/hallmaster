733e70f4d1bd78a579340fa00dd049627c14f6be
DURAND Malo <durand.malo.60590@gmail.com>
2026-02-22 11:42:13 -0500

    feat(backend): /auth/register returns a JWT (#36)
    * feat(backend): /auth/register returns a JWT
    
    * fix: pnpm-lock.yaml was broken

--- packages/backend/src/auth/entities/created-user.entity.ts
+++
@@ -1,4 +0,0 @@
-import { createZodDto } from 'nestjs-zod';
-import { createdUserSchema } from '../schemas/created-user.schema.js';
-
-export class CreatedUserDto extends createZodDto(createdUserSchema) {}
