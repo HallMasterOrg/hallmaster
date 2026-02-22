ea86ece5b51e33d7ae4fcb4730ba536d142546bf
zowks <67444066+zowks@users.noreply.github.com>
2026-02-22 16:48:33 +0000

    feat(frontend-onboarding): added `createBot` api integration

--- packages/frontend/src/lib/remotes/bot.remote.ts
+++ packages/frontend/src/lib/remotes/bot.remote.ts
@@ -1,7 +1,27 @@
 import { CreateBotSchema } from "@hallmaster/backend/dto";
-import { redirect } from "@sveltejs/kit";
-import { form } from "$app/server";
+import { error, redirect } from "@sveltejs/kit";
+import { form, getRequestEvent } from "$app/server";
 
-export const createBot = form(CreateBotSchema, async () => {
-  redirect(303, "/");
+export const createBot = form(CreateBotSchema, async (data) => {
+  const token = getRequestEvent().cookies.get("token");
+
+  const response = await fetch(`/bot`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  switch (response.status) {
+    case 201:
+      return redirect(303, "/");
+    case 401:
+      return error(401, "Unauthorized");
+    case 409:
+      return error(409, "A bot already exists");
+    default:
+      return error(500, "An error occured");
+  }
 });
