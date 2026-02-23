0555ef7a90202bc7b4dbc3defcbce8ac0c90e2ef
zowks <67444066+zowks@users.noreply.github.com>
2026-02-23 07:43:16 +0000

    feat(frontend-onboarding): adds deployment settings when creating bot

--- packages/frontend/src/routes/(form)/setup/bot/components/Deployment.svelte
+++ packages/frontend/src/routes/(form)/setup/bot/components/Deployment.svelte
@@ -1,22 +1,63 @@
 <script lang="ts">
-import { BracesIcon, ContainerIcon } from "@lucide/svelte";
+import {
+  BracesIcon,
+  ContainerIcon,
+  RectangleEllipsisIcon,
+  TextCursorIcon,
+} from "@lucide/svelte";
+import { Switch } from "@skeletonlabs/skeleton-svelte";
 import LabeledInput from "$lib/components/LabeledInput.svelte";
+import { createBot } from "$lib/remotes/bot.remote";
+
+let checked: boolean = $state(false);
+
+const { dockerImage, token } = createBot.fields;
 </script>
 
 <div class="flex flex-col gap-4">
   <LabeledInput
     label="Container image url"
     icon={ContainerIcon}
-    type="url"
-    placeholder="https://ghcr.io/image:tag"
-    disabled
+    placeholder="ghcr.io/image:tag"
+    required
+    {...dockerImage.image.as("text")}
+    issues={dockerImage.image.issues()}
   />
+
+  <Switch {checked} onCheckedChange={(details) => (checked = details.checked)}>
+    <Switch.Control>
+      <Switch.Thumb />
+    </Switch.Control>
+    <Switch.Label>Registry authentication</Switch.Label>
+    <Switch.HiddenInput />
+  </Switch>
+
+  {#if checked}
+    <div class="flex gap-2">
+      <LabeledInput
+        label="Username"
+        icon={TextCursorIcon}
+        required
+        {...dockerImage.username.as("text")}
+        issues={dockerImage.username.issues()}
+      />
+
+      <LabeledInput
+        label="Password"
+        icon={RectangleEllipsisIcon}
+        required
+        {...dockerImage.password.as("password")}
+        issues={dockerImage.password.issues()}
+      />
+    </div>
+  {/if}
 
   <LabeledInput
     label="Bot token"
     icon={BracesIcon}
-    type="password"
     placeholder="8f0a2f30b4e738362ee19e8e572edb8a"
-    disabled
+    required
+    {...token.as("password")}
+    issues={token.issues()}
   />
 </div>
