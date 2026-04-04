dba5d73e310bfdd6317b150047e57da9247c5bfb
A1ex <a1ex@a1ex.fr>
2026-04-03 21:04:02 -0400

    feat(backend): add public decorator

--- packages/backend/src/auth/guards/jwt.guard.ts
+++ packages/backend/src/auth/guards/jwt.guard.ts
@@ -4,12 +4,17 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { FastifyRequest } from 'fastify';
 import { AuthService } from '../auth.service.js';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator.js';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly reflector: Reflector,
+  ) {}
 
   private static extractBearerToken(request: FastifyRequest): string {
     const authHeader = request.headers.authorization ?? '';
@@ -20,6 +25,15 @@ export class AuthGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<FastifyRequest>();
     const token = AuthGuard.extractBearerToken(request);
 
