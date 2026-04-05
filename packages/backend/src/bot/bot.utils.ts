1e4cad442012d71a4a451a415b06084168dc98f3
A1ex <a1ex@a1ex.fr>
2026-04-05 01:07:31 -0400

    chore(backend): prettier file

--- packages/backend/src/bot/bot.utils.ts
+++ packages/backend/src/bot/bot.utils.ts
@@ -1,9 +1,11 @@
-export function formatDockerImage(dockerImage: {
-  serverName: string;
-  image: string;
-  tag: string;
-  username: string | null;
-} | null) {
+export function formatDockerImage(
+  dockerImage: {
+    serverName: string;
+    image: string;
+    tag: string;
+    username: string | null;
+  } | null,
+) {
   if (dockerImage === null) {
     throw new Error('Docker image is required but was not found.');
   }
