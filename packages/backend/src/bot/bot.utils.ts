4fa4440f1ed425741ddb6a73f3c1ecb0cbda4a1f
A1ex <a1ex@a1ex.fr>
2026-04-05 01:05:22 -0400

    fix(backend): check if Docker image is provided

--- packages/backend/src/bot/bot.utils.ts
+++ packages/backend/src/bot/bot.utils.ts
@@ -3,7 +3,11 @@ export function formatDockerImage(dockerImage: {
   image: string;
   tag: string;
   username: string | null;
-}) {
+} | null) {
+  if (dockerImage === null) {
+    throw new Error('Docker image is required but was not found.');
+  }
+
   return {
     image: `${dockerImage.serverName}/${dockerImage.image}:${dockerImage.tag}`,
     username: dockerImage.username,
