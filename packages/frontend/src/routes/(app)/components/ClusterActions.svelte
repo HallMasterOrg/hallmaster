28e2b82e743f74d5d2e076c725077e44c192e629
zowks <67444066+zowks@users.noreply.github.com>
2026-05-15 23:54:15 +0000

    fix(frontend): removed optional prop from `ClusterActions`

--- packages/frontend/src/routes/(app)/components/ClusterActions.svelte
+++ packages/frontend/src/routes/(app)/components/ClusterActions.svelte
@@ -12,11 +12,10 @@
     PlayIcon,
     RotateCcwIcon,
     SquareIcon,
-    TrashIcon,
   } from "@lucide/svelte";
 
   type Props = {
-    id?: GetClusterDto["id"];
+    id: GetClusterDto["id"];
     status: GetClusterDto["status"];
   };
 
@@ -46,34 +45,32 @@
   let loading: (typeof actions)[number]["label"] | null = $state(null);
 </script>
 
-{#if id}
-  {#each actions as { require, remote, icon, label }}
-    {#if require === status}
-      {@const Icon = icon}
+{#each actions as { require, remote, icon, label }}
+  {#if require === status}
+    {@const Icon = icon}
 
-      <button
-        class="btn-icon"
-        onclick={() => {
-          loading = label;
-          remote(id)
-            .catch((error) => {
-              toaster.create({
-                type: "error",
-                title: "Error",
-                description: JSON.parse(error).message,
-              });
-              getClusters().refresh();
-            })
-            .finally(() => (loading = null));
-        }}
-        disabled={loading === label}
-      >
-        {#if loading === label}
-          <LoaderCircleIcon class="animate-spin" />
-        {:else}
-          <Icon />
-        {/if}
-      </button>
-    {/if}
-  {/each}
-{/if}
+    <button
+      class="btn-icon"
+      onclick={() => {
+        loading = label;
+        remote(id)
+          .catch((error) => {
+            toaster.create({
+              type: "error",
+              title: "Error",
+              description: JSON.parse(error).message,
+            });
+            getClusters().refresh();
+          })
+          .finally(() => (loading = null));
+      }}
+      disabled={loading === label}
+    >
+      {#if loading === label}
+        <LoaderCircleIcon class="animate-spin" />
+      {:else}
+        <Icon />
+      {/if}
+    </button>
+  {/if}
+{/each}
