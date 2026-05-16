8ac76e9ece2054f7f0719852e37544d96a2c2621
Zowks <67444066+zowks@users.noreply.github.com>
2026-05-16 14:37:53 +0200

    feat(frontend): add `/settings` page (#83)

--- packages/frontend/src/lib/components/LabeledInput.svelte
+++ packages/frontend/src/lib/components/LabeledInput.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-import { type Icon, TriangleAlertIcon } from "@lucide/svelte";
-import type { RemoteFormIssue } from "@sveltejs/kit";
-import type { HTMLInputAttributes } from "svelte/elements";
+  import { type Icon, TriangleAlertIcon } from "@lucide/svelte";
+  import type { RemoteFormIssue } from "@sveltejs/kit";
+  import type { Snippet } from "svelte";
+  import type { HTMLInputAttributes } from "svelte/elements";
 
-interface Props extends HTMLInputAttributes {
-  label: string;
-  icon?: typeof Icon;
-  issues?: RemoteFormIssue[];
-}
+  interface Props extends HTMLInputAttributes {
+    label: string;
+    icon?: typeof Icon;
+    issues?: RemoteFormIssue[];
+    suffix?: Snippet;
+  }
 
-let { label, icon, issues, ...props }: Props = $props();
+  let { label, icon, issues, suffix, ...props }: Props = $props();
 </script>
 
 <label class="label">
-  <div class={[
-    "ml-1 flex gap-1",
-    issues && "text-error-500"
-  ]}>
+  <div class={["ml-1 flex gap-1", issues && "text-error-500"]}>
     {#if issues}
       <TriangleAlertIcon size={16} />
     {:else if icon}
@@ -25,9 +24,15 @@ let { label, icon, issues, ...props }: Props = $props();
     {/if}
 
     <span class="label-text">
-      {label}<span class="text-primary-500">{props.required && "*"}</span>
+      {label}
+      {#if props.required}
+        <span class="text-primary-500">*</span>
+      {/if}
     </span>
   </div>
 
-  <input class="input" {...props} />
+  <div class="input flex items-center gap-1">
+    <input class="grow" {...props} />
+    {@render suffix?.()}
+  </div>
 </label>
--- packages/frontend/src/lib/remotes/bot.remote.ts
+++ packages/frontend/src/lib/remotes/bot.remote.ts
@@ -1,11 +1,11 @@
-import { command, form, getRequestEvent } from "$app/server";
+import { command, form, getRequestEvent, query } from "$app/server";
 import { env } from "$env/dynamic/private";
-import { CreateBotSchema, UpdateBotSchema } from "@hallmaster/backend/dto";
+import { CreateBotSchema, UpdateBotSchema, type GetBotDto } from "@hallmaster/backend/dto";
 import { error, redirect } from "@sveltejs/kit";
 
 import { getClusters } from "./clusters.remote";
 
-export const createBot = form(CreateBotSchema, async (data) => {
+export const createBot = form(CreateBotSchema, async (payload) => {
   const token = getRequestEvent().cookies.get("token");
 
   const response = await fetch(`${env.API_URL}/bot`, {
@@ -14,7 +14,7 @@ export const createBot = form(CreateBotSchema, async (data) => {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(payload),
   });
 
   switch (response.status) {
@@ -30,7 +30,31 @@ export const createBot = form(CreateBotSchema, async (data) => {
   }
 });
 
-export const updateBotLayout = command(UpdateBotSchema.pick({ layout: true }), async (layout) => {
+export const getBot = query<GetBotDto>(async () => {
+  const token = getRequestEvent().cookies.get("token");
+
+  const response = await fetch(new URL("/bot", env.API_URL), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  switch (response.status) {
+    case 200:
+      return response.json();
+    case 401:
+      return error(401, "Unauthorized");
+    case 404:
+      return error(404, "Bot not found");
+
+    default:
+      return error(500, "An error occurred");
+  }
+});
+
+export const updateBotToken = form(UpdateBotSchema.pick({ token: true }), async (payload) => {
   const token = getRequestEvent().cookies.get("token");
 
   const response = await fetch(new URL("/bot", env.API_URL), {
@@ -39,7 +63,61 @@ export const updateBotLayout = command(UpdateBotSchema.pick({ layout: true }), a
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(layout),
+    body: JSON.stringify(payload),
+  });
+
+  switch (response.status) {
+    case 202:
+      getBot().set(await response.json());
+      return;
+    case 401:
+      return error(401, "Unauthorized");
+    case 404:
+      return error(404, "Bot not found");
+
+    default:
+      console.error(await response.text());
+      return error(500, "An error occurred");
+  }
+});
+
+export const updateBotImage = form(UpdateBotSchema.pick({ dockerImage: true }), async (payload) => {
+  const token = getRequestEvent().cookies.get("token");
+
+  const response = await fetch(new URL("/bot", env.API_URL), {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(payload),
+  });
+
+  switch (response.status) {
+    case 202:
+      getBot().set(await response.json());
+      return;
+    case 401:
+      return error(401, "Unauthorized");
+    case 404:
+      return error(404, "Bot not found");
+
+    default:
+      console.error(await response.text());
+      return error(500, "An error occured");
+  }
+});
+
+export const updateBotLayout = command(UpdateBotSchema.pick({ layout: true }), async (payload) => {
+  const token = getRequestEvent().cookies.get("token");
+
+  const response = await fetch(new URL("/bot", env.API_URL), {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(payload),
   });
 
   switch (response.status) {
